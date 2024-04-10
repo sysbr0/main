ab9e7211c244f848951824e092a007287438da64
sysbr <sysbr@outlook.com>
2024-04-10 22:54:56 +0300

    connect with neo4j databse

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from .neo4j_service import Neo4jService
-from .neo4j_http_service import Neo4jHTTPService
+from neo4j import GraphDatabase
 
-
+from .models import Book
 
 # Create your views here.
 
@@ -18,7 +17,8 @@ from django.shortcuts import render
 
 def navbar(request):
     social_media_links = [
-        {
+    
+         {
             'platform': 'instagram',
             'url': 'https://www.instagram.com/your_instagram_username',
             'icon_class': 'fab fa-instagram fa-lg',
@@ -47,10 +47,7 @@ def navbar(request):
       
     ]
     menu =[ 
-        {
-          'name': 'facebook',
-            'link': 'https://www.facebook.com/your_facebook_page'
-        },
+    
 
           {
           'name': 'facebook',
@@ -68,10 +65,7 @@ def navbar(request):
           'name': 'facebook',
             'link': 'https://www.facebook.com/your_facebook_page'
         },
-          {
-          'name': 'facebook',
-            'link': 'https://www.facebook.com/your_facebook_page'
-        },
+    
      ]
 
 
@@ -96,7 +90,7 @@ def index2(request) :
             'publisher': 'HarperFlamingo Canada',
             'image_url': 'http://images.amazon.com/images/P/0002005018.01.LZZZZZZZ.jpg'
         },
-        # Add more books similarly
+
              ]
      return  render(request, 'pages/index2.html' ,{'books': books})  
 
@@ -130,10 +124,99 @@ def indix(request) :
 
 
 
+def book_list(request):
+  
 
-def my_view(request):
-    neo4j_http_service = Neo4jHTTPService(NEO4J_HTTP_URI, NEO4J_USERNAME, NEO4J_PASSWORD)
-    result = neo4j_http_service.run_query("MATCH (n) RETURN n LIMIT 5")
-    nodes = result['results'][0]['data']
+
+    # Connect to Neo4j
+    neo4j_uri = "bolt://localhost:7687"
+    neo4j_username = "neo4j"
+    neo4j_password = "12345678"
+    driver = GraphDatabase.driver(neo4j_uri, auth=(neo4j_username, neo4j_password))
+   
+    cypher_query = (
+    "MATCH (b:Book) RETURN b LIMIT 25")
+
+     
+    with driver.session() as session:
+        result = session.run(cypher_query)
+        books = []
+        for record in result:
+            book_data = record['b']
+            book = Book(
+                element_id=book_data['<elementId>'],
+                neo4j_id=book_data['<id>'],
+                avg_age=book_data['AvgOfAge'],
+                avg_book_rating=book_data['AvgOfbookRating'],
+                author=book_data['Book-Author'],
+                title=book_data['Book-Title'],
+                isbn=book_data['ISBN'],
+                image_url=book_data['Image-URL-L'],
+                publisher=book_data['Publisher'],
+                year_of_publication=book_data['Year-Of-Publication'],
+                category_id=book_data['catagoryid'],
+                category=book_data['category'],
+                description=book_data['description']
+            )
+            books.append(book)
     
-    return render(request, 'pages/template.html', {'nodes': nodes})
+    return render(request, 'pages/template.html', {'books': books})
+
+    
+
+
+def filter(request):
+   
+
+
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "12345678"))
+    with driver.session() as session:
+        result = session.run("""
+        MATCH (b:Book)
+        RETURN b """)
+        books = [record['b'] for record in result]
+
+
+
+
+    return render(request, 'pages/template.html', {'books': books})
+
+
+def fatch(request):
+    
+    neo4j_uri = "bolt://localhost:7687"
+    neo4j_username = "neo4j"
+    neo4j_password = "12345678"
+    driver = GraphDatabase.driver(neo4j_uri, auth=(neo4j_username, neo4j_password))
+    
+    cypher_query = (
+
+        "MATCH (c:Category {categoryId: 61})<-[:BELONGS_TO]-(book:Book) "
+        "RETURN book, c"
+    )
+    
+    books = []
+    
+    with driver.session() as session:
+        result = session.run(cypher_query)
+        for record in result:
+            book_data = record['book']
+            category_data = record['c']
+            book = Book(
+                element_id=book_data['<elementId>'],
+                neo4j_id=book_data['<id>'],
+                avg_age=book_data['AvgOfAge'],
+                avg_book_rating=book_data['AvgOfbookRating'],
+                author=book_data['Book-Author'],
+                title=book_data['Book-Title'],
+                isbn=book_data['ISBN'],
+                image_url=book_data['Image-URL-L'],
+                publisher=book_data['Publisher'],
+                year_of_publication=book_data['Year-Of-Publication'],
+                category_id=book_data['catagoryid'],
+                category=book_data['category'],
+                description=book_data['description']
+            )
+            books.append(book)
+    
+    return render(request, "pages/template.html", {'books': books})
